6075ef6b4b670ac28498aaf40af5d42e51a63ae7
suhao <638781@qq.com>
2020-03-27 13:03:45 +0800

    Initial commit.

--- head.js
+++ head.js
@@ -1,22 +1,24 @@
 const endpoint = 'https://www.jsonstore.io/d4d04c066563a19493bd5dde589c4dbf20c949657b6bd4cc76f1862af80b5e99'
 const ebox = document.getElementById('erbox')
 const fetchJSON = (a) => {
-	const f = new XMLHttpRequest()
-	f.open('GET', a, false)
-	f.onreadystatechange = oEvent => {
-		if (f.readyState === 4) {
-			if (f.status === 200) {} else {
+	const request = new XMLHttpRequest()
+	request.open('GET', a, false)
+	request.setRequestHeader('Access-Control-Allow-Origin', '*')
+	request.setRequestHeader('Access-Control-Allow-Headers', 'Authorization, Access-Control-Allow-Headers, Origin, Content-Type, Content-Length, Accept')
+	request.onreadystatechange = oEvent => {
+		if (request.readyState === 4) {
+			if (request.status === 200) {} else {
 				ebox.style.display = 'block'
 				ebox.innerHTML = 'network seems to be offline'
 			}
 		}
 	}
-	f.onerror = () => {
+	request.onerror = () => {
 		ebox.style.display = 'block'
 		ebox.innerHTML = 'network error'
 	}
-	f.send()
-	return f.responseText
+	request.send()
+	return request.responseText
 }
 const isURL = (a) => {
 	const f = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
